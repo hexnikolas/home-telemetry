1e55727be217353abc1aef2a4cfc9f532c3b1275
Nikos <nikos.zacharatos@iccs.gr>
2026-03-08 23:44:47 +0200

    added correct image and extension creation to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,11 @@ from sqlalchemy.pool import NullPool
 from app.models import Base
 from app.database import init_engine
 from app.main import app
-
+from sqlalchemy import text
 
 @pytest.fixture(scope="session")
 def postgres_container():
-    with PostgresContainer("postgres:15") as pg:
+    with PostgresContainer("timescale/timescaledb:latest-pg16") as pg:
         yield pg
 
 
@@ -30,6 +30,7 @@ def init_test_engine(db_url):
 async def setup_tables(db_url):
     engine = create_async_engine(db_url, poolclass=NullPool)
     async with engine.begin() as conn:
+        await conn.execute(text("CREATE EXTENSION IF NOT EXISTS timescaledb;"))
         await conn.run_sync(Base.metadata.create_all)
     yield
     async with engine.begin() as conn:
